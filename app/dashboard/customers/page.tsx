05491c347fdaa1dfce1239285933757b7d406058
heinnthantsin <heinnthant01@gmail.com>
2024-03-28 23:29:09 +0630

    first cmt

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function page() {
+  return <div>This is customers dashboard</div>;
+}
+
+export default page;
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <>
+      <h1>Hello World</h1>
+      <p>This is Dashboard Page</p>
+    </>
+  );
+}
